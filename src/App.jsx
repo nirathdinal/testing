f2d47fbf3dbccdd0cc40c18c34f864c206050caa
nirathdinal <nirathdinal2004@gmail.com>
2026-09-13 11:55:30 +0530

    test 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Footer from './Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,19 @@
+function Footer(){
+	return(
+	<footer>
+	<h2>Hello world</h2>
+    <p>&copy;Your website name</p>
+
+	<div className="card">
+		<p className="inside">
+			my name is <br />
+			my location <br />
+			my address <br />
+		</p>
+	</div>
+	   
+	</footer>
+	);
+}
+
+export default Footer
